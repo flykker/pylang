dfa60ba9cb5a2011bbbac6ffb1b4669e1f1c60ea
Your Name <you@example.com>
2024-02-27 12:05:12 +0300

    up main loop socket

--- tests/main.py
+++ tests/main.py
@@ -65,12 +65,12 @@ def exit(code: int) -> void:
 def bytearray(count: int) -> ptr[str]:
     pass
 
-# def fib(n):
-#     if n < 2:
-#         return n
-#     else:
-#         return fib(n-1) + fib(n-2)
-#     return 0
+def fib(n):
+    if n < 2:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+    return 0
 
 # def pytest(n):
 #     b = [111,222,333,444,555]
@@ -85,67 +85,67 @@ def bytearray(count: int) -> ptr[str]:
 #     print(333)
 #     return 0
 
-# class Socket:
-#     sock: int
+class Socket:
+    sock: int
 
-#     def create(self):
-        # AF_INET = 2
-        # SOCK_STREAM = 1
+    def create(self):
+        AF_INET = 2
+        SOCK_STREAM = 1
 
-        # sock = socket(2, 1, 0)
-        # print(sock)
-        # print()
+        sock = socket(2, 1, 0)
+        print(sock)
+        print()
 
-        # if sock == -1:
-        #     print("Error create socket")
-        #     print()
+        if sock == -1:
+            print("Error create socket")
+            print()
+            exit(0)
+        
+        sock_addr = sockaddr_in()
+        sock_addr.sin_family = i16(2)
+        sock_addr.sin_port = htons(i16(8000))
+        sock_addr.sin_addr = inet_addr('127.0.0.1')
+        sock_addr.sin_zero = i64(0)
+        
+        len = sizeof(sock_addr)
+
+        b = bind(sock, sock_addr, len)
+        if b < 0:
+            print("Error bind socket")
+            print()
+            exit(0)
+        
+        print('Bind socket success')
+        print()
+        
+        listen(sock, 10)
+        
+        flags = 0
+        loop = 1
+        buf_size = 1024
+        output = bytearray(buf_size)
+        #server = "HTTP/1.1 200 OK\nServer: Z-Server\n"
+        server = ""
+        
+        # while loop == 1:
+        newsockfd = accept(sock, sock_addr, len)
+        recv_data = 1         
+
+        # if newsockfd < 0:
+        #     print("Error webserver accept")
         #     exit(0)
         
-        # sock_addr = sockaddr_in()
-        # sock_addr.sin_family = i16(2)
-        # sock_addr.sin_port = htons(i16(8000))
-        # sock_addr.sin_addr = inet_addr('127.0.0.1')
-        # sock_addr.sin_zero = i64(0)
-        
-        # len = sizeof(sock_addr)
+        print('Accept connect\n')
 
-        # b = bind(sock, sock_addr, len)
-        # if b < 0:
-        #     print("Error bind socket")
-        #     print()
-        #     exit(0)
-        
-        # print('Bind socket success')
-        # print()
-        
-        # listen(sock, 10)
-        
-        # flags = 0
-        # loop = 1
-        # buf_size = 1024
-        # output = bytearray(buf_size)
-        # #server = "HTTP/1.1 200 OK\nServer: Z-Server\n"
-        # server = ""
-        
-        # # while loop == 1:
-        # newsockfd = accept(sock, sock_addr, len)
-        # recv_data = 1         
-
-        # # if newsockfd < 0:
-        # #     print("Error webserver accept")
-        # #     exit(0)
-        
-        # print('Accept connect\n')
-
-        # recv_msg_size = recv(newsockfd, output, i64(buf_size), flags)
-        # #print(output)
+        recv_msg_size = recv(newsockfd, output, i64(buf_size), flags)
+        #print(output)
         
         
-        # # send(newsockfd, server, 33, flags)
-        # send(newsockfd, server, i64(33), flags)
-        # close(newsockfd)
-        # close(sock)
-        # return 0
+        # send(newsockfd, server, 33, flags)
+        send(newsockfd, server, i64(33), flags)
+        close(newsockfd)
+        close(sock)
+        return 0
 
 # comments
 def main():
@@ -166,8 +166,8 @@ def main():
 
     b = bind(sock, sock_addr, len)
     
-    # print('Bind socket success')
-    # print()
+    print('Bind socket success')
+    print()
     
     listen(sock, 10)
     
@@ -175,16 +175,18 @@ def main():
     loop = 1
     buf_size = 1024
     output = bytearray(buf_size)
-    server = "HTTP/1.1 200 OK\nServer: Z-Server\n"
+    server = "HTTP/1.1 200 OK\nServer: Z-Server\nContent-type: text/html\n\n<html><body>Z-Server</body></html>"
     
-    newsockfd = accept(sock, sock_addr, len)      
+    while loop == 1:
+        newsockfd = accept(sock, sock_addr, len)      
 
-    print('Accept connect\n')
+        print('Accept connect\n')
 
-    recv_msg_size = recv(newsockfd, output, i64(buf_size), flags)
-    print(output)
+        recv_msg_size = recv(newsockfd, output, i64(buf_size), flags)
+        print(output)
 
-    send(newsockfd, server, i64(33), flags)
-    close(newsockfd)
+        send(newsockfd, server, i64(96), flags)
+        close(newsockfd)
+
     close(sock)
     return 0
